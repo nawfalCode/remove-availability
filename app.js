b96fc2238b88c72da6f44c24113599d2bd25e1ea
nawfalalicode <nawfal.ali@monash.edu>
2025-02-23 02:22:52 +1100

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const functions = require('@google-cloud/functions-framework');
+// const { onRequest } = require("firebase-functions/v2/https");
+const { onCall } = require('firebase-functions/v2/https');
 
-// Register a CloudEvent callback with the Functions Framework that will
-// be executed when the Pub/Sub trigger topic receives a message.
-functions.cloudEvent('helloPubSub', cloudEvent => {
-  // The Pub/Sub message is passed as the CloudEvent's data payload.
-  const base64name = cloudEvent.data.message.data;
 
-  const name = base64name
-    ? Buffer.from(base64name, 'base64').toString()
-    : 'World';
+exports.getUsers = onCall(async ({ data, auth }) => {
 
-  console.log(`Hello, ${name}!`);
-});
+  return ({ data: 'Welcome Nawfal' });
+}
+);
+
